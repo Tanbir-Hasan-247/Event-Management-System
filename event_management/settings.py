b6bc6c0d3757559fa3d93b8e7561457c8cdbc83b
Tanbir-Hasan-247 <tanbirhasan2117@gmail.com>
2026-04-02 18:33:58 +0600

    add requirements

--- event_management/settings.py
+++ event_management/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-j*a2_%a9!e145eafack8ms6=x!fsiwqxx)00(b@wbc+@osgtyu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["http://*.onrender.com", "http://127.0.0.1:8000"]
 
 
 # Application definition
